9006a2862231eec1b9b176c5b38db022da310c11
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-11-08 11:35:43 +0500

    Initial commit

---
+++ src/main/java/org/example/dockerconfigmetubemicroservice/DockerConfigMetubeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.dockerconfigmetubemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerConfigMetubeMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerConfigMetubeMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dockerconfigmetubemicroservice/DockerConfigMetubeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+//package org.example.dockerconfigmetubemicroservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DockerConfigMetubeMicroserviceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
